deced97ae4167d0fc61f42abd15971e1679e9845
weifengxian <502995228@qq.com>
2018-11-16 23:15:25 +0800

    Update Person.java
    rrrrr

--- src/proxy/dynamic/Person.java
+++ src/proxy/dynamic/Person.java
@@ -3,7 +3,7 @@ package proxy.dynamic;
  * 顶层接口
  * */
 public interface Person {
-	//uuuu
+	//uuuuIIIIIIIIIIOOOOOO
 	public void sayHello(String content, int age);
 	//heool
 	public void sayGoodBye(boolean seeAgin, double time);
